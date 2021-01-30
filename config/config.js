92da8869608a4f04e01a634a56b7ee26a80e13bc
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2021-01-30 11:43:16 +0700

    Add media model and migration

--- config/config.js
+++ config/config.js
@@ -1,32 +1,25 @@
-require('dotenv').config();
-
-const {
-  APP_DB_NAME,
-  APP_DB_USERNAME,
-  APP_DB_PASSWORD,
-  APP_DB_HOSTNAME
-} = process.env;
+require('dotenv').config(); // this is important!
 
 module.exports = {
-  "development": {
-    "username": APP_DB_USERNAME",
-    "password": APP_DB_PASSWORD,
-    "database": APP_DB_NAME,
-    "host": APP_DB_HOSTNAME,
+"development": {
+    "username": process.env.APP_DB_USERNAME,
+    "password": process.env.APP_DB_PASSWORD,
+    "database": process.env.APP_DB_DATABASE,
+    "host": process.env.APP_DB_HOSTNAME,
     "dialect": "mysql"
-  },
-  "test": {
-    "username": APP_DB_USERNAME",
-    "password": APP_DB_PASSWORD,
-    "database": APP_DB_NAME,
-    "host": APP_DB_HOSTNAME,
+},
+"test": {
+    "username": process.env.APP_DB_USERNAME,
+    "password": process.env.APP_DB_PASSWORD,
+    "database": process.env.APP_DB_DATABASE,
+    "host": process.env.APP_DB_HOSTNAME,
     "dialect": "mysql"
-  },
-  "production": {
-    "username": APP_DB_USERNAME",
-    "password": APP_DB_PASSWORD,
-    "database": APP_DB_NAME,
-    "host": APP_DB_HOSTNAME,
+},
+"production": {
+    "username": process.env.APP_DB_USERNAME,
+    "password": process.env.APP_DB_PASSWORD,
+    "database": process.env.APP_DB_DATABASE,
+    "host": process.env.APP_DB_HOSTNAME,
     "dialect": "mysql"
-  }
 }
+};
